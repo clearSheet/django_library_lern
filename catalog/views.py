7f9966df333dff4a0f4b64d240584094ebf11499
wooziee <clearSheet@gmail.com>
2023-04-12 18:00:10 +0300

    add count visited page for any user

--- catalog/views.py
+++ catalog/views.py
@@ -21,6 +21,10 @@ def index(request):
     # Количество жанров
     num_genre = Genre.objects.all().count()
 
+    num_authors = Author.objects.count()  # The 'all()' is implied by default.
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
     # Отрисовка HTML-шаблона index.html с данными внутри
     # переменной контекста context
     return render(
@@ -31,7 +35,8 @@ def index(request):
             'num_instances': num_instances,
             'num_instances_available': num_instances_available,
             'num_authors': num_authors,
-            'num_genre': num_genre},
+            'num_genre': num_genre,
+            'num_visits':num_visits},
     )
 
 
